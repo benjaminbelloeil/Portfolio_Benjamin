a30ae021794949874218a455c968ce5040261896
Benjamin <benjaminbelloeil03@gmail.com>
2024-05-06 01:04:41 -0600

    Adding all necessary files

---
+++ src/data/constants.js
@@ -0,0 +1,356 @@
+import IBlogo from '../images/IBlogo.png';
+import HarritonLogo from '../images/HarritonLogo.png';
+import TINSALogo from '../images/TINSALogo.png';
+import BonLogo from '../images/BonLogo.png';
+import FrenchLogo from '../images/FrenchLogo.png';
+import SenioritasLogo from '../images/SenioritasLogo.png';
+import TECLogo from '../images/TECLogo.png';
+import todoWeb from '../images/todoWeb.png';
+import usfinternational from '../images/usfinterWeb.png';
+
+
+
+export const Bio = {
+  name: "Benjamin Belloeil",
+  roles: [
+    "Software Engineer",
+    "Web Developer",
+    "Tech Student",
+    "Programmer",
+  ],
+  description:
+    "As a student aspiring to become a software engineer, I'm driven by a passion for learning and a thirst for tackling new coding challenges. I'm dedicated to consistently delivering high-quality code and solutions. With a growth mindset and a positive attitude, I'm excited to contribute meaningfully to the world of software development and achieve success in this field.",
+  github: "https://github.com/benjaminbelloeil",
+  resume: "https://drive.google.com/file/d/1p16iUnoYFucP3J4TaHJnd7jmKxaQbOYI/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/benjamin-belloeil-15396b254/",
+  insta: "https://www.instagram.com/benjaminbelloeil_/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "R",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/1b/R_logo.svg",
+      },
+      {
+        name: "C++",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Apple",
+    skills: [
+      {
+        name: "Swift",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swift/swift-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "Dart",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Xcode",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xcode/xcode-original.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Playgrounds",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swift/swift-original.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+      {
+          name: "MySQLWorkbench",
+          image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: TINSALogo,
+    role: "Technical Support Intern",
+    company: "Tuberia Industrial del Norte - TINSA",
+    date: "Nov 2023 - Present",
+    desc: [ "Provide technical support for the seamless migration of an ERP software system within a high-volume commercial enterprise with over 9,000 unique products.",
+            "Collaborate closely with cross-functional teams to ensure a smooth transition and integration of the ERP software, optimizing operational efficiency and accuracy.",
+            "Troubleshoot and resolve any software-related issues, ensuring minimal disruption to critical business processes during the migration phase.",
+            "Deliver assistance to end-users, empowering them to effectively utilize the new ERP system and maximize its potential benefits.",
+    ],
+    skills: [
+      "Technical Support",
+      "Collaboration",
+      "Troubleshooting",
+      "Communication",
+      "Problem-solving",
+      "End-user Training",
+      "Integration",
+      "Operational Efficiency",
+      "Software Issue Resolution",
+      "Customer Service",
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: BonLogo,
+    role: "Volunteer Firefighter",
+    company: "Bon Air Fire Co",
+    date: "Sep 2021 - Oct 2023",
+    desc: [ "Initiated basic training at Bon Air Fire Company, acquiring essential life skills and knowledge.",
+            "Sharpened communication and problem-solving skills significantly through hands-on experiences.",
+            "Participated in various emergency response operations, including fire suppression, search and rescue, and medical assistance.",
+            "Collaborated with a diverse team of professionals to ensure the safety and well-being of the community.",
+            "Emphasized and enhanced time management skills, fostering efficiency and effective use of time.",
+
+    ],
+    skills: [
+      "Communication",
+      "Problem-solving",
+      "Emergency Response",
+      "Teamwork",
+      "Collaboration",
+      "Leadership",
+      "Life Skills",
+      "Time Management",
+      "Safety Awareness",
+      "Community Service",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: FrenchLogo,
+    role: "Teaching Assistant",
+    company: "PhilaFLAM",
+    date: "Sep 2021 - Jun 2022",
+  desc: ["Supported classroom activities and organized materials for a group of five to nine-year-old students.",
+          "Assisted in the preparation of lesson plans and activities, ensuring a dynamic and engaging learning environment.",
+          "Provided one-on-one support to students, helping them understand and complete assignments.",
+          "Facilitated group discussions and activities, fostering a collaborative and interactive learning experience.",
+          "Significantly enhanced personal speaking proficiency through hands-on teaching experience in a French classroom setting.",
+          "Assisted the instructor in grading classwork and assessments, contributing to the overall learning process.",
+  ],
+    skills: [
+      "Organization",
+      "Preparation",
+      "Support",
+      "Engagement",
+      "Collaboration",
+      "Communication",
+      "Proficiency",
+      "Patience",
+      "Creativity",
+      "Leadership",
+    ],
+  },
+  {
+    id: 2,
+    img: SenioritasLogo,
+    role: "Server/ Manager",
+    company: "Señorita's Mexican Taqueria",
+    date: "Jun 2021 - Jun 2022",
+    desc: ["Developed exceptional communication skills in both English and Spanish through customer service and phone interactions at Señorita's Mexican Taqueria.",
+            "Managed a team of servers, ensuring a smooth and efficient dining experience for customers.",
+            "Enhanced problem-solving skills by addressing customer concerns and resolving issues in a timely and effective manner.",
+            "Mastered efficient multitasking and organizational abilities by seamlessly managing diverse tasks, including table service, takeout orders, and deliveries, optimizing overall operational efficiency and customer satisfaction.",
+            "Demonstrated leadership skills by training new employees and providing guidance to ensure consistent service quality.",
+            "Cultivated strong memorization skills critical for order accuracy and a seamless dining experience.",
+  ],
+    skills: [
+      "Memorization",
+      "Efficiency",
+      "Training",
+      "Organization",
+      "Multitasking",
+      "Leadership",
+      "Communication",
+      "Adaptability",
+      "Responsibility",
+      "Detail-oriented",
+      "Initiative",
+      "Teamwork",
+    ],
+    doc: " ",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: TECLogo,
+    school: "Tecnológico de Monterrey - Mexico",
+    date: "Aug 2022 - Jun 2026",
+    grade: "88.17%",
+    desc: "I am presently enrolled in the Bachelor's program for Computer Technology Engineering at Tecnológico de Monterrey, Mexico. Having successfully completed four semesters, I maintain an impressive average of 88.17%. Throughout my academic journey, I have delved into various foundational subjects such as Data Structures, Algorithms, Object-Oriented Programming, and Computer Networks, among others. These courses have not only broadened my understanding of core concepts but have also equipped me with practical skills essential for tackling real-world challenges in the field of technology.",
+    degree: "Bachelor's in Computer Technology Engineering"
+  },
+  {
+    id: 1,
+    img: IBlogo,
+    school: "International Baccalaureate Diploma - USA",
+    date: "Sep 2020 - Jun 2022",
+    grade: "29 Points",
+    desc: "I completed my International Baccalaureate Diploma at Harriton High School in the USA. During my studies, I explored various subjects such as Mathematics, English, Biology, and History, alongside Higher level courses in Spanish and French. This comprehensive program not only expanded my knowledge but also sharpened my critical thinking, research, and communication skills. I achieved a score of 29 points, reflecting my commitment to academic excellence and my ability to succeed in a challenging academic setting.",
+    degree: "International Baccalaureate Diploma",
+  },
+  {
+    id: 2,
+    img: HarritonLogo,
+    school: "Harriton High School - USA",
+    date: "Sep 2018 - Jun 2022",
+    grade: "3.55 GPA",
+    desc: "I earned my high school diploma from Harriton High School in the USA with a GPA of 3.55",
+    degree: "High School Diploma",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "To-Do List",
+    description:
+      "This simple to-do list is a clean and efficient way to organize your tasks. Built using HTML, JavaScript, and CSS, it offers a straightforward interface for managing your daily activities. With its minimalist design, the focus remains on productivity without unnecessary distractions.",
+    image: 
+      todoWeb,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/benjaminbelloeil/ToDo-List",
+  },
+  {
+    id: 0,
+    title: "Login Page",
+    description:
+      "This login page, crafted with Tailwind CSS and React JS, offers a sleek and modern interface for user authentication. Seamlessly integrating Tailwind's utility-first approach with React's component-based structure, it provides a smooth user experience.",
+    image:
+      "https://private-user-images.githubusercontent.com/113941255/323857287-9d3b48ea-ef0a-4a9a-8478-72b67e47c987.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQ5NzYwNjksIm5iZiI6MTcxNDk3NTc2OSwicGF0aCI6Ii8xMTM5NDEyNTUvMzIzODU3Mjg3LTlkM2I0OGVhLWVmMGEtNGE5YS04NDc4LTcyYjY3ZTQ3Yzk4Ny5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNTA2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDUwNlQwNjA5MjlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kZmY4MGZjOTVhMTFiZDkwMTQ0M2E3YzA3NzY5ZThlNmM4N2Y5YjQ5ZmJmYWY3MjBkYWU0OWY0Zjk4MGFjMDgxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.pzg-6Y_3SZu6LLZqMQKDf8FLsseabb-CW6banvA5OEU",
+    tags: [
+      "React Js", 
+      "JavaScript", 
+      "Tailwindcss", 
+      "HTML", 
+      "CSS"
+    ],
+    category: "web app",
+    github: "https://github.com/benjaminbelloeil/Login-Page",
+  },
+  {
+    id: 1,
+    title: "ufsinternational",
+    description:
+      "Nuestra plataforma de servicio social está diseñada para abordar desafíos globales alineados con los Objetivos de Desarrollo Sostenible (ODS) de la Organización de las Naciones Unidas (ONU). Esta iniciativa integra los principios y valores de los ODS para promover un impacto positivo en comunidades locales y globales.",
+    image: usfinternational,
+    tags: [
+      "Tailwindcss",
+      "JavaScript",
+      "Node Js",
+      "Express Js",
+      "TypeScript",
+      "CSS",
+      "Next js",
+      "Shadcn"
+    ],
+    category: "web app",
+    github: "https://github.com/benjaminbelloeil/ufsinternational",
+    webapp: "",
+  },
+  {
+    id: 2,
+    title: "Rockwell Website",
+    description:
+      "Sistema de software basado en modelo cliente-servidor, implementado en web con capacidad de conexión a una base de datos relacional. Con visualización de indicadores de control basada en técnicas de videojuegos (tableros 2D, 3D, múltiples cámaras) para el apoyo a la toma de decisiones en la solución de un problema específico de una empresa (socio formador).",
+    image:
+      "https://private-user-images.githubusercontent.com/113941255/327661338-5fc84bde-fd3e-4204-90cf-6418d3543f25.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQ5NzY5MTYsIm5iZiI6MTcxNDk3NjYxNiwicGF0aCI6Ii8xMTM5NDEyNTUvMzI3NjYxMzM4LTVmYzg0YmRlLWZkM2UtNDIwNC05MGNmLTY0MThkMzU0M2YyNS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNTA2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDUwNlQwNjIzMzZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MGM3OWZmOGRkMzYyNWE1NGIyNTZmMTRiZTE5NDNlMGE1YzkwMjI2NDUwMDZhNmYxMWI3N2VhN2E1ODNmYzU2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.-vWF5lm2WIDnk4_Jr4VHaKnTWlw5S9aqLE9EzGu1ovw",
+    tags: ["Next js", "JavaScript", "Tailwindcss", "CSS", "Express Js"],
+    category: "machine learning",
+    github: "https://github.com/benjaminbelloeil/RockwellWeb",
+    webapp: "",
+  },
+];
+
+
---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
+  },
+  // Colors for layout
+  colors: {
+    primary1: "#0080FF", // Vibrant Blue
+    background1: "#000000", // Black
+    button: "#0080FF", // Vibrant Blue
+    background2: "#000000", // Black
+    text: "#0080FF", // Vibrant Blue
+    text1: "#000000", // Black
+    text2: "#0080FF", // Vibrant Blue
+    text3: "#000000", // Black
+    footerBackground: "#000000" // Black
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+};
